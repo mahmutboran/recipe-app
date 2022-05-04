8067a5c0bfb5a4399e8f52a948511c5c028191d2
mahmutboran <86852026+mahmutboran@users.noreply.github.com>
2022-05-04 09:02:19 +0300

    first

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./LoginStyles";
+
+import meal from "../../assets/meal.svg";
+
+const Login = () => {
+
+  const user = {
+
+    username:"user"
+  } 
+
+
+  const handleSubmit = (e) =>{
+
+    e.preventDefault()
+
+    sessionStorage.setItem("user",JSON.stringify(user))
+    window.location.href="/home"
+  }
+
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} alt="meal" />
+        <Header>{"<Clarusway/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit} >
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="text" placeholder="password" required />
+          <StyledButton type="submit" >Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
